9f6cd2023773e27202f0e86c81629b0114fbf71e
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-12-04 13:34:44 +0000

    Deploying to gh-pages from @ Natali258/bc-66-practice-js-parcel@43224c6f45a877ecd6d09d789c6e822bebd75787 🚀

--- form.1898e8be.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-contact-form").addEventListener("input",(function(e){var t=e.target,o=t.name,a=t.value,r=localStorage.getItem("feedbackForm");console.log(r),(r=r?JSON.parse(r):{})[o]=a,localStorage.setItem("feedbackForm",JSON.stringify(r))}));
-//# sourceMappingURL=form.1898e8be.js.map
