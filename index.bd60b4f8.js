8c48644224a3523f286c2c5c0618a23415e84446
VasylPopaz <145382665+VasylPopaz@users.noreply.github.com>
2023-12-06 12:51:24 +0000

    Deploying to gh-pages from @ Natali258/bc-66-practice-js-parcel@3d06ac021b629fea72758c68d451bc248b163046 🚀

---
+++ index.bd60b4f8.js
@@ -0,0 +1,2 @@
+let o="Poly";var e;(e="Mango",console.log("start fetch"),new Promise(((o,l)=>{setTimeout((()=>{Math.random()<.5&&o(["Mango","mango@gmail.com"]),l(`${e} not found`)}),1e3)}))).then((([e,l])=>(console.log(e,l),o=e,e))).then((o=>{console.log(o)})).catch((o=>{console.log(o)})).finally((()=>{console.log("end fetch")})),console.log(o);
+//# sourceMappingURL=index.bd60b4f8.js.map
