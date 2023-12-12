dd3dc147f2c35f048627fdc3546973efc0288687
VasylPopaz <pafos1997@gmail.com>
2023-12-12 14:30:45 +0100

    task 2

--- src/js/contacts/contacts.js
+++ src/js/contacts/contacts.js
@@ -1,10 +1,38 @@
-import { getContacts } from './contacts-api';
+import Notiflix, { Notify } from 'notiflix';
+import { createContact, deleteContact, getContacts } from './contacts-api';
 import { createListMarkup } from './create-markup';
 
 const refs = {
+  form: document.querySelector('.js-contact-form'),
   list: document.querySelector('.js-list'),
 };
+
 getContacts().then(data => {
   const markup = createListMarkup(data);
   refs.list.innerHTML = markup;
 });
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const contact = {};
+  const formData = new FormData(event.target);
+  formData.forEach((value, key) => {
+    contact[key] = value;
+  });
+  createContact(contact).then(data => {
+    const markup = createListMarkup([data]);
+    refs.list.insertAdjacentHTML('afterbegin', markup);
+  });
+};
+
+refs.form.addEventListener('submit', handleSubmit);
+
+const onDeleteContact = event => {
+  if (event.target.nodeName !== 'BUTTON') return;
+  const oldElem = event.target.closest('LI');
+  deleteContact(event.target.dataset.id).then(data => {
+    oldElem.remove();
+    Notify.success(`${data.name} deleted`);
+  });
+};
+refs.list.addEventListener('click', onDeleteContact);
--- src/js/contacts/create-markup.js
+++ src/js/contacts/create-markup.js
@@ -2,10 +2,12 @@ export const createListMarkup = data => {
   return data
     .map(
       contact => `<li class="contacts-item">
+      <span>${contact.id}</span>
     <p>${contact.name}</p>
         <p>${contact.email}</p>
         <p>${contact.number}</p>
-        <button type="button">Delete</button>
+      
+        <button type="button" data-id="${contact.id}">Delete</button>
         </li>`
     )
     .join('');
