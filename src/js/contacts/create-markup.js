891f4cce7e5e6f9607866eccd0c82cf644ad9564
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2023-12-14 14:41:03 +0200

    qan finnall task 1

--- src/js/contacts/create-markup.js
+++ src/js/contacts/create-markup.js
@@ -1,14 +1,24 @@
 export const createListMarkup = data => {
   return data
     .map(
-      contact => `<li class="contacts-item">
+      contact => `<li class="contacts-item"  data-id="${contact.id}">
       <span>${contact.id}</span>
     <p>${contact.name}</p>
         <p>${contact.email}</p>
         <p>${contact.number}</p>
       
-        <button type="button" data-id="${contact.id}">Delete</button>
+        <button type="button">Delete</button>
         </li>`
     )
     .join('');
+};
+
+export const createContactInfo = ({ id, name, email, number }) => {
+  return `<div class="modal-item"  data-id="${id}">
+  <button class="modal-close-btn">X</button>
+      <span>${id}</span>
+    <p>${name}</p>
+        <p>${email}</p>
+        <p>${number}</p>
+        </div>`;
 };
